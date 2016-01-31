2e42ae26c499e8b93cbac569c9342efeb25925e6
mgi166 <skskoari@gmail.com>
2016-01-31 22:48:20 +0900

    Use process.env.PWD in context
    a* Fix not found babel-loader problem.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var path = require('path');
+var process = require('process');
 
 module.exports = {
-  context: "./frontend",
+  context: path.join(process.env.PWD, 'frontend'),
   entry: "./index.js",
   output: {
     path: path.join(__dirname, 'dist'),
