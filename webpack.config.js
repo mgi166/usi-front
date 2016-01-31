de193ac90dde1ecf3db7aad961a67ceb5688843d
mgi166 <skskoari@gmail.com>
2016-01-31 22:43:23 +0900

    Fix lint of object

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 var path = require('path');
 
 module.exports = {
-  "context": "./frontend",
-  "entry": "./index.js",
-  "output": {
-    "path": path.join(__dirname, 'dist'),
-    "filename": 'webpack.bundle.js'
+  context: "./frontend",
+  entry: "./index.js",
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'webpack.bundle.js'
   },
   module: {
     loaders: [
