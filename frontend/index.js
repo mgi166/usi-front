fa23b01baab39e10bc07ca20de02bce849eb19f2
mgi166 <skskoari@gmail.com>
2016-02-01 21:55:59 +0900

    import express from 'express'
    * set externals and target, avoid to error.

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,4 @@
-import React from "react";
+import path from 'path';
+import React from 'react';
+import express from 'express';
+import config from '../webpack.config.js';
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 var path = require('path');
 var process = require('process');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
 
 module.exports = {
   context: path.join(process.env.PWD, 'frontend'),
   entry: "./index.js",
+  target: 'node',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'webpack.bundle.js'
   },
+  externals: nodeModules,
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel' }
