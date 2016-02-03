61f1331004e1604e3856e89504e4e469cbd4904a
mgi166 <skskoari@gmail.com>
2016-02-03 23:35:30 +0900

    Fix ` ReferenceError: process is not defined` on ReactPref
    * ReactPref call `process`, but does not defined.
      so webpack.DefinePlugin give process object.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var process = require('process');
 var fs = require('fs');
+var webpack = require('webpack');
 
 module.exports = {
   context: path.join(process.env.PWD, 'frontend'),
@@ -10,6 +11,13 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'webpack.bundle.js'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ],
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel' }
