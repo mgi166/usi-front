d7d5b4b78096851f1a036d0052c840a813db24e5
mgi166 <skskoari@gmail.com>
2016-02-04 22:16:09 +0900

    React.component -> React.Component

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Board extends React.component {
+export default class Board extends React.Component {
   render() {
     return(
       <h2>Board</h2>
