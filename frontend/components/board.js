823f4a061ceda074632365f7d58dfaf76b8bf2b9
mgi166 <skskoari@gmail.com>
2016-02-07 14:52:58 +0900

    Create <table> component.

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -2,14 +2,29 @@ import React from 'react';
 import boardImage from "../images/shogi_board/ban_kaya_d.png";
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+    var board = [];
+
+    for (var i = 1; i <= 9; i++) {
+      board.push(['', '', '', '', '', '', '', '', '']);
+    }
+
+    this.state = { board: board };
+  }
+
   render() {
+    var tbody = this.state.board.map((row, y) => {
+      var rows = row.map((piece, x) => {
+        return(<td key={x}>{x + 1},{y + 1}</td>);
+      });
+      return(<tr key={y}>{rows}</tr>);
+    });
+
     return(
-        <table className="board" background={boardImage}>
-        <tbody>
-        <tr>
-          <td>aaa</td>
-        </tr>
-        </tbody>
+      <table className="board" background={boardImage}>
+        <tbody>{tbody}</tbody>
       </table>
     );
   }
