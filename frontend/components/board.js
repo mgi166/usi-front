a8082bf30c3f0e79d930f0af8ddc7b6b74862801
mgi166 <skskoari@gmail.com>
2016-02-07 13:39:59 +0900

    [wip] Use background image
    * but does not display image, why!!!!

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import boardImage from "../images/shogi_board/ban_kaya_d.png";
 
 export default class Board extends React.Component {
   render() {
     return(
-      <h2>Board</h2>
+        <table className="board" background={boardImage}>
+        <tbody>
+        <tr>
+          <td>aaa</td>
+        </tr>
+        </tbody>
+      </table>
     );
   }
 }
