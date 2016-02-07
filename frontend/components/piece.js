b685112e94385335ff80a57bd099c6a297d69999
mgi166 <skskoari@gmail.com>
2016-02-07 16:38:16 +0900

    Add piece class
    * take responsibility of piece of shogi.

---
+++ frontend/components/piece.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Piece extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="piece">
+      </div>
+    );
+  }
+}
