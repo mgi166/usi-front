c5f175e7ced7a6ad12050776c0aea0205c951ffb
mgi166 <skskoari@gmail.com>
2016-02-07 16:38:32 +0900

    Use Piece class in board

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import boardImage from "../images/shogi_board/ban_kaya_d.png";
+import Piece from './piece';
 
 export default class Board extends React.Component {
   constructor(props) {
@@ -25,9 +26,12 @@ export default class Board extends React.Component {
     });
 
     return(
-      <table className="board" background={boardImage}>
-        <tbody>{tbody}</tbody>
-      </table>
+      <div>
+        <table className="board" background={boardImage}>
+          <tbody>{tbody}</tbody>
+        </table>
+        <Piece />
+      </div>
     );
   }
 }
