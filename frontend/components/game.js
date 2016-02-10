4f594ca474f2ebe45666e811391a752df7dccaf7
mgi166 <skskoari@gmail.com>
2016-02-10 09:59:04 +0900

    Add Game components
    * Wrap Board component for manage `turn`

--- frontend/components/game.js
+++ frontend/components/game.js
@@ -1,5 +1,12 @@
-export default class Game {
-  constructor(board) {
-    this.board = board;
+import React from 'react';
+import Board from './board';
+
+export default class Game extends React.Component {
+  render () {
+    return (
+      <div className="game">
+        <Board />
+      </div>
+    );
   }
 }
