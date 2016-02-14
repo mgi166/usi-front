c6da5a3691b27ae8c3cd63c3b489512a0c3e19e0
mgi166 <skskoari@gmail.com>
2016-02-14 12:29:26 +0900

    move board status to store.
    * Store is singleton via Redux

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -2,28 +2,12 @@ import React from 'react';
 import boardImage from "../images/shogi_board/ban_kaya_d.png";
 import Piece from './piece';
 import Shogi from '../src/shogi';
+import store from '../stores/index';
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
-
-    var board = [
-      ['l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l'],
-      ['*', 'b', '*', '*', '*', '*', '*', 'r', '*'],
-      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-      ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
-      ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
-      ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
-      ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-      ['*', 'B', '*', '*', '*', '*', '*', 'R', '*'],
-      ['L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L']
-    ];
-
-    this.state = { board: board };
-  }
-
   render() {
-    var tbody = this.state.board.map((row, y) => {
+    var board = store.getState().board;
+    var tbody = board.map((row, y) => {
       var yCor = y + 1;
       var rows = row.map((piece, x) => {
         var xCor = 10 - x - 1;
--- frontend/stores/index.js
+++ frontend/stores/index.js
@@ -2,7 +2,18 @@ import { createStore } from 'redux';
 import changeTurn from '../reducers';
 
 const initialState = {
-  turn: 'black'
+  turn: 'black',
+  board: [
+    ['l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l'],
+    ['*', 'b', '*', '*', '*', '*', '*', 'r', '*'],
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+    ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+    ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    ['*', 'B', '*', '*', '*', '*', '*', 'R', '*'],
+    ['L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L']
+  ]
 };
 
 let store = createStore(changeTurn, initialState);
