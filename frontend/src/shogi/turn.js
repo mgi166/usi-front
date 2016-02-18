c63aa49b59dcc975bba60df8f6a0b6b1cb972267
mgi166 <skskoari@gmail.com>
2016-02-19 00:22:18 +0900

    Add showReadableTurn func.
    * return human readable turn name.

--- frontend/src/shogi/turn.js
+++ frontend/src/shogi/turn.js
@@ -2,4 +2,15 @@ export default class Turn {
   constructor() {
     this.turn = 'black';
   }
+
+  showReadableTurn() {
+    switch(this.turn) {
+    case 'black':
+      return '先手';
+    case 'white':
+      return '後手';
+    default:
+      return undefined;
+    }
+  }
 }
