dcaa437956cd212d4e4d9ea1c953854185054fde
mgi166 <skskoari@gmail.com>
2016-02-19 09:58:14 +0900

    Add usiMoveDef function.
    * return move definition each types

--- frontend/src/shogi/piece.js
+++ frontend/src/shogi/piece.js
@@ -17,6 +17,53 @@ export default class Piece {
     return this.type;
   }
 
+  usiMoveDef() {
+    switch (this.type) {
+    case 'p':
+      return { just: [[0, -1]] };
+    case 'P':
+      return { just: [[0, 1]] };
+    case 'l':
+      return { fly: [[0, -1]] };
+    case 'L':
+      return { fly: [[0, 1]] };
+    case 'n':
+      return { just: [[-1, -2], [1, -2]] };
+    case 'N':
+      return { just: [[-1, 2], [1, 2]] };
+    case 's':
+      return { just: [[-1, -1], [-1, 0], [-1, 1], [1, -1], [1, 1]] };
+    case 'S':
+      return { just: [[1, -1], [1, 0], [1, 1], [-1, -1], [-1, 1]] };
+    case 'g':
+    case 'p+':
+    case 'l+':
+    case 'n+':
+    case 's+':
+      return { just: [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, 0]] };
+    case 'G':
+    case 'P+':
+    case 'L+':
+    case 'N+':
+    case 'S+':
+      return { just: [[1, -1], [1, 0], [1, 1], [0, -1], [0, 1], [-1, 0]] };
+    case 'r':
+    case 'R':
+      return { fly: [[0, -1], [0, 1], [1, 0], [-1, 0]] };
+    case 'b':
+    case 'B':
+      return { fly: [[1, 1], [1, -1], [-1, 1], [-1, -1]] };
+    case 'r+':
+    case 'R+':
+      return { fly: [[1, 1], [1, -1], [-1, 1], [-1, -1]], just: [[1, -1], [1, 1], [-1, 1], [-1, -1]] };
+    case 'b+':
+    case 'B+':
+      return { fly: [[1, 1], [1, -1], [-1, 1], [-1, -1]], just: [[1, -1], [1, 1], [-1, 1], [-1, -1]] };
+    default:
+      return undefined;
+    }
+  }
+
   usiPromoteTypes() {
     return {
       p: 'p+',
