84c186be115bdfc06a286c70af538a1c44a98e57
mgi166 <skskoari@gmail.com>
2016-02-20 17:33:47 +0900

    Add `change prop of piece that is moved` test
    * fail this test now.

--- test/src/board_test.js
+++ test/src/board_test.js
@@ -1,1 +1,41 @@
-import Piece from '../../frontend/src/shogi/board';
+import Board from '../../frontend/src/shogi/board';
+import Piece from '../../frontend/src/shogi/piece';
+import sinon from 'sinon';
+import memo from 'memo-is';
+
+describe('Borad', () => {
+  describe('#enhanceMovablePoint', () => {
+    context('porn', () => {
+      var board = memo().is(() => {
+        return (
+          [
+            ['l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l'],
+            ['*', 'b', '*', '*', '*', '*', '*', 'r', '*'],
+            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+            ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+            ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+            ['*', '*', '*', '*', '*', '*', '*', 'P', '*'],
+            ['P', 'P', 'P', 'P', 'P', 'P', 'P', '*', 'P'],
+            ['*', 'B', '*', '*', '*', '*', '*', 'R', '*'],
+            ['L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L']
+          ]
+        );
+      });
+
+      it('change property of piece that is moved', () => {
+        var testBoard = new Board;
+        var piece = new Piece('P', 2, 6);
+        testBoard.board = board;
+        testBoard.enhanceMovablePoint(piece);
+
+        var result = [];
+
+        var movable = testBoard.board().forEach((row) => {
+          var cell = row.filter((cell) => { return(cell.isMovable); });
+          if (cell.length) { result.push(cell); }
+        });
+        result.should.eql([new Piece('*', 2, 5)]);
+      });
+    });
+  });
+});
