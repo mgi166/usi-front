495574f1666cffcd23297ed5733d2b19370c741c
mgi166 <skskoari@gmail.com>
2016-02-21 11:27:10 +0900

    Add test examples
    * Similer to black test.
    * These test is fail now.

--- test/src/board_test.js
+++ test/src/board_test.js
@@ -78,9 +78,67 @@ describe('Borad', () => {
 
     context('case of white', () => {
       context('exists movable coordinates', () => {
+        var board = memo().is(() => {
+          var _board = new Board;
+          _board.setBoard(position());
+          return(_board);
+        });
+
+        var position = memo().is(() => {
+          return (
+            [
+              ['*', 'p', '*'],
+              ['*', '*', '*']
+            ]
+          );
+        });
+
+        it('change property of piece that is moved', () => {
+          var piece = new Piece({ type: 'p', x: 8, y: 1 });
+
+          board().enhanceMovablePoint(piece);
+
+          var movablePieces = board().board.map((row) => {
+            return (
+              row.filter((cell) => { return(cell.movable); })
+            );
+          });
+
+          _.flattenDeep(movablePieces).should.eql(
+            [new Piece({ type: '*', x: 8, y: 2, movable: true })]
+          );
+        });
       });
 
       context('does not exist movable coordinates', () => {
+        var board = memo().is(() => {
+          var _board = new Board;
+          _board.setBoard(position());
+          return(_board);
+        });
+
+        var position = memo().is(() => {
+          return (
+            [
+              ['*', '*', '*'],
+              ['p', '*', '*']
+            ]
+          );
+        });
+
+        it('does not change property of piece', () => {
+          var piece = new Piece({ type: 'p', x: 9, y: 2 });
+
+          board().enhanceMovablePoint(piece);
+
+          var movablePieces = board().board.map((row) => {
+            return (
+              row.filter((cell) => { return(cell.movable); })
+            );
+          });
+
+          _.flattenDeep(movablePieces).should.eql([]);
+        });
       });
 
       context.skip('if move piece, king is taken', () => {
