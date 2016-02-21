498c32667a77fc07285ddbbf34e418f9e70837a9
mgi166 <skskoari@gmail.com>
2016-02-21 12:02:29 +0900

    Fix context and add new spec

--- test/src/board_test.js
+++ test/src/board_test.js
@@ -7,7 +7,73 @@ describe('Borad', () => {
   describe('#enhanceMovablePoint', () => {
     context('porn', () => {
       context('case of black', () => {
-        context('exists movable coordinates', () => {
+        context('match the piece to coordinate', () => {
+          context('exists movable coordinates', () => {
+            var board = memo().is(() => {
+              var _board = new Board;
+              _board.setBoard(position());
+              return(_board);
+            });
+
+            var position = memo().is(() => {
+              return (
+                [
+                  ['*', '*', '*'],
+                  ['*', 'P', '*']
+                ]
+              );
+            });
+
+            it('change property of piece that is moved', () => {
+              var piece = new Piece({ type: 'P', x: 8, y: 2 });
+
+              board().enhanceMovablePoint(piece);
+
+              var movablePieces = board().board.map((row) => {
+                return (
+                  row.filter((cell) => { return(cell.movable); })
+                );
+              });
+
+              _.flattenDeep(movablePieces).should.eql(
+                [new Piece({ type: '*', x: 8, y: 1, movable: true })]
+              );
+            });
+          });
+
+          context('does not exist movable coordinates', () => {
+            var board = memo().is(() => {
+              var _board = new Board;
+              _board.setBoard(position());
+              return(_board);
+            });
+
+            var position = memo().is(() => {
+              return (
+                [
+                  ['P', '*', '*'],
+                  ['*', '*', '*']
+                ]
+              );
+            });
+
+            it('does not change property of piece', () => {
+              var piece = new Piece({ type: 'P', x: 9, y: 1 });
+
+              board().enhanceMovablePoint(piece);
+
+              var movablePieces = board().board.map((row) => {
+                return (
+                  row.filter((cell) => { return(cell.movable); })
+                );
+              });
+
+              _.flattenDeep(movablePieces).should.eql([]);
+            });
+          });
+        });
+
+        context('does not match the piece to coordinate', () => {
           var board = memo().is(() => {
             var _board = new Board;
             _board.setBoard(position());
@@ -18,46 +84,13 @@ describe('Borad', () => {
             return (
               [
                 ['*', '*', '*'],
-                ['*', 'P', '*']
+                ['*', '*', 'P']
               ]
             );
           });
 
-          it('change property of piece that is moved', () => {
+          it('throw exception', () => {
             var piece = new Piece({ type: 'P', x: 8, y: 2 });
-
-            board().enhanceMovablePoint(piece);
-
-            var movablePieces = board().board.map((row) => {
-              return (
-                row.filter((cell) => { return(cell.movable); })
-              );
-            });
-
-            _.flattenDeep(movablePieces).should.eql(
-              [new Piece({ type: '*', x: 8, y: 1, movable: true })]
-            );
-          });
-        });
-
-        context('does not exist movable coordinates', () => {
-          var board = memo().is(() => {
-            var _board = new Board;
-            _board.setBoard(position());
-            return(_board);
-          });
-
-          var position = memo().is(() => {
-            return (
-              [
-                ['P', '*', '*'],
-                ['*', '*', '*']
-              ]
-            );
-          });
-
-          it('does not change property of piece', () => {
-            var piece = new Piece({ type: 'P', x: 9, y: 1 });
 
             board().enhanceMovablePoint(piece);
 
