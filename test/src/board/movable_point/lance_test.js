20ba9b777292c4f74c28e942801ea928af6b7a9b
mgi166 <skskoari@gmail.com>
2016-02-22 00:01:53 +0900

    Add example but this is failed.

--- test/src/board/movable_point/lance_test.js
+++ test/src/board/movable_point/lance_test.js
@@ -6,6 +6,45 @@ import _ from 'lodash';
 describe('black', () => {
   context('match the piece of coordinate', () => {
     context('exists movable coordinates', () => {
+      var board = memo().is(() => {
+        var _board = new Board;
+        _board.setBoard(position());
+        return(_board);
+      });
+
+      var position = memo().is(() => {
+        return (
+          [
+            ['*', '*', '*'],
+            ['*', '*', '*'],
+            ['*', '*', '*'],
+            ['*', '*', '*'],
+            ['*', 'L', '*'],
+            ['*', '*', '*']
+          ]
+        );
+      });
+
+      it('change property of piece is movable', () => {
+        var piece = new Piece({ type: 'L', x: 8, y: 5 });
+
+        board().enhanceMovablePoint(piece);
+
+        var movablePieces = board().board.map((row) => {
+          return (
+            row.filter((cell) => { return(cell.movable); })
+          );
+        });
+
+        _.flattenDeep(movablePieces).should.eql(
+          [
+            new Piece({ type: '*', x: 8, y: 1, movable: true }),
+            new Piece({ type: '*', x: 8, y: 2, movable: true }),
+            new Piece({ type: '*', x: 8, y: 3, movable: true }),
+            new Piece({ type: '*', x: 8, y: 4, movable: true }),
+          ]
+        );
+      });
     });
 
     context('does not exist movable coordinates', () => {
