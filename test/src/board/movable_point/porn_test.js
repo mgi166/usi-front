0d24b62df53c4cb85e542af07247dd1aa79210d1
mgi166 <skskoari@gmail.com>
2016-02-21 23:51:08 +0900

    Add white case

--- test/src/board/movable_point/porn_test.js
+++ test/src/board/movable_point/porn_test.js
@@ -98,7 +98,73 @@ describe('black', () => {
 });
 
 describe('white', () => {
-  context('exists movable coordinates', () => {
+  context('match the piece to coordinate', () => {
+    context('exists movable coordinates', () => {
+      var board = memo().is(() => {
+        var _board = new Board;
+        _board.setBoard(position());
+        return(_board);
+      });
+
+      var position = memo().is(() => {
+        return (
+          [
+            ['*', 'p', '*'],
+            ['*', '*', '*']
+          ]
+        );
+      });
+
+      it('change property of piece that is moved', () => {
+        var piece = new Piece({ type: 'p', x: 8, y: 1 });
+
+        board().enhanceMovablePoint(piece);
+
+        var movablePieces = board().board.map((row) => {
+          return (
+            row.filter((cell) => { return(cell.movable); })
+          );
+        });
+
+        _.flattenDeep(movablePieces).should.eql(
+          [new Piece({ type: '*', x: 8, y: 2, movable: true })]
+        );
+      });
+    });
+
+    context('does not exist movable coordinates', () => {
+      var board = memo().is(() => {
+        var _board = new Board;
+        _board.setBoard(position());
+        return(_board);
+      });
+
+      var position = memo().is(() => {
+        return (
+          [
+            ['*', '*', '*'],
+            ['p', '*', '*']
+          ]
+        );
+      });
+
+      it('does not change property of piece', () => {
+        var piece = new Piece({ type: 'p', x: 9, y: 2 });
+
+        board().enhanceMovablePoint(piece);
+
+        var movablePieces = board().board.map((row) => {
+          return (
+            row.filter((cell) => { return(cell.movable); })
+          );
+        });
+
+        _.flattenDeep(movablePieces).should.eql([]);
+      });
+    });
+  });
+
+  context('mismatch the piece to coordinate', () => {
     var board = memo().is(() => {
       var _board = new Board;
       _board.setBoard(position());
@@ -108,57 +174,16 @@ describe('white', () => {
     var position = memo().is(() => {
       return (
         [
-          ['*', 'p', '*'],
+          ['*', '*', 'p'],
           ['*', '*', '*']
         ]
       );
     });
 
-    it('change property of piece that is moved', () => {
-      var piece = new Piece({ type: 'p', x: 8, y: 1 });
+    it('throw exception', () => {
+      var piece = new Piece({ type: 'p', x: 9, y: 1 });
 
-      board().enhanceMovablePoint(piece);
-
-      var movablePieces = board().board.map((row) => {
-        return (
-          row.filter((cell) => { return(cell.movable); })
-        );
-      });
-
-      _.flattenDeep(movablePieces).should.eql(
-        [new Piece({ type: '*', x: 8, y: 2, movable: true })]
-      );
-    });
-  });
-
-  context('does not exist movable coordinates', () => {
-    var board = memo().is(() => {
-      var _board = new Board;
-      _board.setBoard(position());
-      return(_board);
-    });
-
-    var position = memo().is(() => {
-      return (
-        [
-          ['*', '*', '*'],
-          ['p', '*', '*']
-        ]
-      );
-    });
-
-    it('does not change property of piece', () => {
-      var piece = new Piece({ type: 'p', x: 9, y: 2 });
-
-      board().enhanceMovablePoint(piece);
-
-      var movablePieces = board().board.map((row) => {
-        return (
-          row.filter((cell) => { return(cell.movable); })
-        );
-      });
-
-      _.flattenDeep(movablePieces).should.eql([]);
+      (() => { return board().enhanceMovablePoint(piece); }).should.throw();;
     });
   });
 
