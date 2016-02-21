d211891bb87cc39fa1e15e883d13cedb2a81332f
mgi166 <skskoari@gmail.com>
2016-02-21 09:47:16 +0900

    Use context. but example does not change

--- test/src/board_test.js
+++ test/src/board_test.js
@@ -6,38 +6,42 @@ import memo from 'memo-is';
 describe('Borad', () => {
   describe('#enhanceMovablePoint', () => {
     context('porn', () => {
-      var board = memo().is(() => {
-        return (
-          [
-            ['l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l'],
-            ['*', 'b', '*', '*', '*', '*', '*', 'r', '*'],
-            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-            ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
-            ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
-            ['*', '*', '*', '*', '*', '*', '*', 'P', '*'],
-            ['P', 'P', 'P', 'P', 'P', 'P', 'P', '*', 'P'],
-            ['*', 'B', '*', '*', '*', '*', '*', 'R', '*'],
-            ['L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L']
-          ]
-        );
-      });
+      context('case of black', () => {
+        context('exists movable coordinates', () => {
+          var board = memo().is(() => {
+            return (
+              [
+                ['l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l'],
+                ['*', 'b', '*', '*', '*', '*', '*', 'r', '*'],
+                ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+                ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+                ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+                ['*', '*', '*', '*', '*', '*', '*', 'P', '*'],
+                ['P', 'P', 'P', 'P', 'P', 'P', 'P', '*', 'P'],
+                ['*', 'B', '*', '*', '*', '*', '*', 'R', '*'],
+                ['L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L']
+              ]
+            );
+          });
 
-      it('change property of piece that is moved', () => {
-        var testBoard = new Board;
-        var piece = new Piece({ type: 'P', x: 2, y: 6 });
+          it('change property of piece that is moved', () => {
+            var testBoard = new Board;
+            var piece = new Piece({ type: 'P', x: 2, y: 6 });
 
-        testBoard.setBoard(board);
-        testBoard.enhanceMovablePoint(piece);
+            testBoard.setBoard(board);
+            testBoard.enhanceMovablePoint(piece);
 
-        var result = [];
-        var movable = testBoard.board.forEach((row) => {
-          var cell = row.filter((cell) => { return(cell.movable); });
-          if (cell.length) { result.push(cell); }
+            var result = [];
+            var movable = testBoard.board.forEach((row) => {
+              var cell = row.filter((cell) => { return(cell.movable); });
+              if (cell.length) { result.push(cell); }
+            });
+
+            [].concat.apply([], result).should.eql(
+              [new Piece({ type: '*', x: 2, y: 5, movable: true })]
+            );
+          });
         });
-
-        [].concat.apply([], result).should.eql(
-          [new Piece({ type: '*', x: 2, y: 5, movable: true })]
-        );
       });
     });
   });
