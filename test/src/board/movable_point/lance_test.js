76355fbbc9b6d5223d3a4509ae41d9f6b8100f20
mgi166 <skskoari@gmail.com>
2016-02-21 23:56:50 +0900

    Add lance test. but empty.
    * TODO

---
+++ test/src/board/movable_point/lance_test.js
@@ -0,0 +1,23 @@
+import Board from '../../../../frontend/src/shogi/board';
+import Piece from '../../../../frontend/src/shogi/piece';
+import memo from 'memo-is';
+import _ from 'lodash';
+
+describe('black', () => {
+  context('match the piece of coordinate', () => {
+    context('exists movable coordinates', () => {
+    });
+
+    context('does not exist movable coordinates', () => {
+    });
+
+    context('other piece exists', () => {
+    });
+  });
+
+  context('mismatch the piece of coordinate', () => {
+  });
+
+  context.skip('if move piece, king is taken', () => {
+  });
+});
