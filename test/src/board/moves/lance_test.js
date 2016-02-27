4b40ba7fa98190bd6506e21aedd999884ed53de1
mgi166 <skskoari@gmail.com>
2016-02-27 19:38:57 +0900

    Add test `enhancePlaceablePoint`.

--- test/src/board/moves/lance_test.js
+++ test/src/board/moves/lance_test.js
@@ -294,6 +294,43 @@ describe('white', () => {
 describe('#enhancePlaceablePoint', () => {
   describe('black', () => {
     context('the normal pattern', () => {
+      var board = memo().is(() => {
+        var _board = new Board;
+        _board.setBoard(position());
+        return(_board);
+      });
+
+      var position = memo().is(() => {
+        return (
+          [
+            ['*', '*', '*'],
+            ['R', '*', '*'],
+            ['*', '*', '*']
+          ]
+        );
+      });
+
+      it('change property of piece that is placeable', () => {
+        var piece = new Piece({ type: 'L', x: 0, y: 0});
+
+        board().enhancePlaceablePoint(piece);
+
+        var placeablePieces = board().board.map((row) => {
+          return (
+            row.filter((cell) => { return(cell.isPlaced); })
+          );
+        });
+
+        _.flattenDeep(placeablePieces).should.eql(
+          [
+            new Piece({ type: '*', x: 8, y: 2, isPlaced: true}),
+            new Piece({ type: '*', x: 7, y: 2, isPlaced: true}),
+            new Piece({ type: '*', x: 9, y: 3, isPlaced: true}),
+            new Piece({ type: '*', x: 8, y: 3, isPlaced: true}),
+            new Piece({ type: '*', x: 7, y: 3, isPlaced: true}),
+          ]
+        );
+      });
     });
   });
 
