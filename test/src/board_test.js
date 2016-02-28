c0a78cdf2d2225454ebb9144a6c71f4f4aad1b4b
mgi166 <skskoari@gmail.com>
2016-02-28 10:41:55 +0900

    Add `isTakenKing` test, but example is empty.
    * TODO

--- test/src/board_test.js
+++ test/src/board_test.js
@@ -2,3 +2,11 @@ import Board from '../../frontend/src/shogi/board';
 import Piece from '../../frontend/src/shogi/piece';
 import memo from 'memo-is';
 import _ from 'lodash';
+
+describe('isTakenKing', () => {
+  context("if take the piece, king is taken", () => {
+  });
+
+  context("if moves the piece, king is taken", () => {
+  });
+});
