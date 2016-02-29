4cc649241b8c092c1d3e9fc7b683eb1327d7fb25
mgi166 <skskoari@gmail.com>
2016-02-29 09:53:53 +0900

    [wip] isTakenKing test

--- test/src/board_test.js
+++ test/src/board_test.js
@@ -1,10 +1,30 @@
 import Board from '../../frontend/src/shogi/board';
 import Piece from '../../frontend/src/shogi/piece';
+import Turn from '../../frontend/src/shogi/turn';
 import memo from 'memo-is';
 import _ from 'lodash';
 
 describe('isTakenKing', () => {
   context("if take the piece, king is taken", () => {
+    var board = memo().is(() => {
+      var _board = new Board;
+      _board.setBoard(position());
+      return(_board);
+    });
+
+    var position = memo().is(() => {
+      return (
+        [
+          ['*', 'p+', '*'],
+          ['*', 'g', '*'],
+          ['*', 'K', '*'],
+        ]
+      );
+    });
+
+    it('returns true', () => {
+      board.isTakenKing(new Turn).should.eql(true);
+    });
   });
 
   context("if moves the piece, king is taken", () => {
