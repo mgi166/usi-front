82f15255d2f0fcd57eeb5945f5648b2ea3bad609
mgi166 <skskoari@gmail.com>
2016-02-29 22:31:57 +0900

    Use `mapStateToProps`, `mapDispatchToProps`

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import boardImage from "../images/shogi_board/ban_kaya_d.png";
 import Piece from './piece';
-import Shogi from '../src/shogi';
-import store from '../stores/index';
+import { connect } from 'react-redux';
+import { holdPiece } from '../actions';
 
-export default class Board extends React.Component {
+const mapStateToProps = (state) => {
+  return { board: state.board };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return (
+    {
+      onBoardClick: (piece) => {
+        return dispatch(holdPiece(piece));
+      }
+    }
+  );
+};
+
+export default class ShogiBoard extends React.Component {
   render() {
-    var tbody = store.getState().board.map((row, y) => {
+    var tbody = this.props.board.map((row, y) => {
       var rows = row.map((piece, x) => {
         return(
           <td key={x}>
-            <Piece piece={piece} onClick={() => { store.dispatch({type: 'HOLD_PIECE', x: x, y: y }); }}/>
+            <Piece piece={piece} onClick={this.props.onBoardClick()}/>
           </td>
         );
       });
