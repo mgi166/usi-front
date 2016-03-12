c97c5ef0038a891ff2f6a66373063b93381ca450
mgi166 <skskoari@gmail.com>
2016-03-12 19:20:26 +0900

    Add #invertCor test.

--- test/src/board_test.js
+++ test/src/board_test.js
@@ -30,3 +30,41 @@ describe('isTakenKing', () => {
   context.skip("if moves the piece, king is taken", () => {
   });
 });
+
+describe('#invertCor', () => {
+  var board = memo().is(() => {
+    var _board = new Board;
+    _board.setBoard(position());
+    return(_board);
+  });
+
+  var position = memo().is(() => {
+    return (
+      [
+        ['l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l'],
+        ['*', 'b', '*', '*', '*', '*', '*', 'r', '*'],
+        ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+        ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+        ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+        ['*', '*', '*', '*', '*', '*', '*', '*', '*'],
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        ['*', 'B', '*', '*', '*', '*', '*', 'R', '*'],
+        ['L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L']
+      ]
+    );
+  });
+
+  it('pawn with 2, 7', () => {
+    var [x, y] = board().invertCor(2, 7);
+    board().board[y][x].type.should.eql('P');
+    board().board[y][x].x.should.eql(2);
+    board().board[y][x].y.should.eql(7);
+  });
+
+  it('* with 3, 6', () => {
+    var [x, y] = board().invertCor(3, 6);
+    board().board[y][x].type.should.eql('*');
+    board().board[y][x].x.should.eql(3);
+    board().board[y][x].y.should.eql(6);
+  });
+});
