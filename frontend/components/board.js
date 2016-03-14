42d4cf5ae8aa67604f1ee7180cd5ec3840415833
mgi166 <skskoari@gmail.com>
2016-03-14 14:40:24 +0900

    Remove unnecessary action

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -2,7 +2,6 @@ import React from 'react';
 import boardImage from "../images/shogi_board/ban_kaya_d.png";
 import Piece from './piece';
 import { connect } from 'react-redux';
-import { movePiece } from '../actions';
 
 const mapStateToProps = (state) => {
   return { board: state.board.board };
