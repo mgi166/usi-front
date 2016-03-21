28d53996ff103fe3765a582d755b41bfc5143fa0
mgi166 <skskoari@gmail.com>
2016-03-21 17:51:58 +0900

    Override constructor in lance

--- frontend/src/shogi/pieces/lance.js
+++ frontend/src/shogi/pieces/lance.js
@@ -2,4 +2,16 @@ import Piece from '../piece';
 import * as CONST from '../constants/pieceTypes';
 
 export default class Lance extends Piece {
+  constructor({ type, x, y, movable = false, isPlaced =  false }) {
+    super({ type, x, y, movable, isPlaced });
+
+    if (this.type !== CONST.USI_LANCE_BLACK_TYPE &&
+        this.type !== CONST.USI_LANCE_WHITE_TYPE &&
+        this.type !== CONST.USI_LANCE_BLACK_PROMOTE_TYPE &&
+        this.type !== CONST.USI_LANCE_WHITE_PROMOTE_TYPE) {
+      throw new Error(`Unexpected type as Lance: type is ${this.type}`);
+    }
+
+    return this;
+  }
 }
--- test/src/pieces/lance_test.js
+++ test/src/pieces/lance_test.js
@@ -3,4 +3,25 @@ import memo from 'memo-is';
 import _ from 'lodash';
 
 describe('Lance', () => {
+  describe('#constructor', () => {
+    context('black', () => {
+      it('this.type is `L`', () => {
+        var lance = new Lance({ type: 'L' });
+        lance.should.have.property('type', 'L');
+      });
+    });
+
+    context('white', () => {
+      it('this.type is `l`', () => {
+        var lance = new Lance({ type: 'l' });
+        lance.should.have.property('type', 'l');
+      });
+    });
+
+    context('unknown type', () => {
+      it('throw exception', () => {
+        (() => { new Lance({ type: '*' }); }).should.throw();
+      });
+    });
+  });
 });
