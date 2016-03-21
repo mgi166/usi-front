c2e11dcf876af26744651eb62be2c2769464e20c
mgi166 <skskoari@gmail.com>
2016-03-21 12:32:05 +0900

    Upper

--- frontend/src/shogi/constants/pieceTypes.js
+++ frontend/src/shogi/constants/pieceTypes.js
@@ -1,4 +1,5 @@
-export const USI_WHITE_PIECES = ['p', 'l', 'n', 's', 'g', 'k', 'r', 'b'];
 export const USI_BLACK_PIECES = ['P', 'L', 'N', 'S', 'G', 'K', 'R', 'B'];
+export const USI_WHITE_PIECES = ['p', 'l', 'n', 's', 'g', 'k', 'r', 'b'];
+
 export const USI_BLACK_TEAM = 'black';
 export const USI_WHITE_TEAM = 'white';
