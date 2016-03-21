d7a15b9e2e23f11cd607fe2ee0217b2c4a31cf56
mgi166 <skskoari@gmail.com>
2016-03-21 18:07:30 +0900

    Add Gold class inherits from Piece.

--- frontend/src/shogi/pieces/gold.js
+++ frontend/src/shogi/pieces/gold.js
@@ -2,4 +2,14 @@ import Piece from '../piece';
 import * as CONST from '../constants/pieceTypes';
 
 export default class Gold extends Piece {
+  constructor({ type, x, y, movable = false, isPlaced =  false }) {
+    super({ type, x, y, movable, isPlaced });
+
+    if (this.type !== CONST.USI_GOLD_BLACK_TYPE &&
+        this.type !== CONST.USI_GOLD_WHITE_TYPE) {
+      throw new Error(`Unexpected type as Gold: type is ${this.type}`);
+    }
+
+    return this;
+  }
 }
--- test/src/pieces/gold_test.js
+++ test/src/pieces/gold_test.js
@@ -3,4 +3,25 @@ import memo from 'memo-is';
 import _ from 'lodash';
 
 describe('Gold', () => {
+  describe('#constructor', () => {
+    context('black', () => {
+      it('this.type is `G`', () => {
+        var gold = new Gold({ type: 'G' });
+        gold.should.have.property('type', 'G');
+      });
+    });
+
+    context('white', () => {
+      it('this.type is `g`', () => {
+        var gold = new Gold({ type: 'g' });
+        gold.should.have.property('type', 'g');
+      });
+    });
+
+    context('unknown type', () => {
+      it('throw exception', () => {
+        (() => { new Gold({ type: '*' }); }).should.throw();
+      });
+    });
+  });
 });
