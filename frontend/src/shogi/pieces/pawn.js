88cd1519148ac84cb17a67d809b51a98e67d72bd
mgi166 <skskoari@gmail.com>
2016-03-21 12:45:38 +0900

    Refactor constructor

--- frontend/src/shogi/pieces/pawn.js
+++ frontend/src/shogi/pieces/pawn.js
@@ -5,11 +5,7 @@ export default class Pawn extends Piece {
   constructor({ type, x, y, movable = false, isPlaced =  false }) {
     super({ type, x, y, movable, isPlaced });
 
-    if (this.isBlack()) {
-      this.type = CONST.USI_PAWN_BLACK_TYPE;
-    } else if (this.isWhite()) {
-      this.type = CONST.USI_PAWN_WHITE_TYPE;
-    } else {
+    if (this.type !== CONST.USI_PAWN_BLACK_TYPE && this.type !== CONST.USI_PAWN_WHITE_TYPE) {
       throw new Error(`Unexpected type as Pawn: type is ${this.type}`);
     }
 
