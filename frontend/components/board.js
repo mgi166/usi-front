9261c8a15e1e33b4f43e9b4eacf666ed65ffa5cd
mgi166 <skskoari@gmail.com>
2016-03-21 11:20:07 +0900

    Remove unnecessary `import`

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import boardImage from "../images/shogi_board/ban_kaya_d.png";
 import Piece from './piece';
 import { connect } from 'react-redux';
 import Styles from '../styles/board.css';
