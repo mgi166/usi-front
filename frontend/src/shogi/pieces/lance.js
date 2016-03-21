0e639975c42bf68a1ecca9e47f4a144f6af5ed9a
mgi166 <skskoari@gmail.com>
2016-03-21 17:49:34 +0900

    Add Lance class inherits from Piece.

---
+++ frontend/src/shogi/pieces/lance.js
@@ -0,0 +1,5 @@
+import Piece from '../piece';
+import * as CONST from '../constants/pieceTypes';
+
+export default class Lance extends Piece {
+}
---
+++ test/src/pieces/lance_test.js
@@ -0,0 +1,6 @@
+import Lance from '../../../frontend/src/shogi/pieces/lance';
+import memo from 'memo-is';
+import _ from 'lodash';
+
+describe('Lance', () => {
+});
