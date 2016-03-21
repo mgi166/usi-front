c5ebcd634d28d862a8d1d24beb942feb3b7c9a30
mgi166 <skskoari@gmail.com>
2016-03-21 13:25:37 +0900

    Override constructor

--- test/src/pieces/knight_test.js
+++ test/src/pieces/knight_test.js
@@ -3,4 +3,25 @@ import memo from 'memo-is';
 import _ from 'lodash';
 
 describe('Knight', () => {
+  describe('#constructor', () => {
+    context('black', () => {
+      it('this.type is `N`', () => {
+        var knight = new Knight({ type: 'N' });
+        knight.should.have.property('type', 'N');
+      });
+    });
+
+    context('white', () => {
+      it('this.type is `n`', () => {
+        var knight = new Knight({ type: 'n' });
+        knight.should.have.property('type', 'n');
+      });
+    });
+
+    context('unknown type', () => {
+      it('throw exception', () => {
+        (() => { new Knight({ type: '*' }); }).should.throw();
+      });
+    });
+  });
 });
