4f296abe6619a59e16327ebdf9a7a5899060ca2b
mgi166 <skskoari@gmail.com>
2016-03-21 13:50:06 +0900

    Override constructor on Silver.

--- frontend/src/shogi/pieces/silver.js
+++ frontend/src/shogi/pieces/silver.js
@@ -2,4 +2,16 @@ import Piece from '../piece';
 import * as CONST from '../constants/pieceTypes';
 
 export default class Silver extends Piece {
+  constructor({ type, x, y, movable = false, isPlaced =  false }) {
+    super({ type, x, y, movable, isPlaced });
+
+    if (this.type !== CONST.USI_SILVER_BLACK_TYPE &&
+        this.type !== CONST.USI_SILVER_WHITE_TYPE &&
+        this.type !== CONST.USI_SILVER_BLACK_PROMOTE_TYPE &&
+        this.type !== CONST.USI_SILVER_WHITE_PROMOTE_TYPE) {
+      throw new Error(`Unexpected type as Silver: type is ${this.type}`);
+    }
+
+    return this;
+  }
 }
--- test/src/pieces/silver_test.js
+++ test/src/pieces/silver_test.js
@@ -3,4 +3,25 @@ import memo from 'memo-is';
 import _ from 'lodash';
 
 describe('Silver', () => {
+  describe('#constructor', () => {
+    context('black', () => {
+      it('this.type is `S`', () => {
+        var silver = new Silver({ type: 'S' });
+        silver.should.have.property('type', 'S');
+      });
+    });
+
+    context('white', () => {
+      it('this.type is `s`', () => {
+        var silver = new Silver({ type: 's' });
+        silver.should.have.property('type', 's');
+      });
+    });
+
+    context('unknown type', () => {
+      it('throw exception', () => {
+        (() => { new Silver({ type: '*' }); }).should.throw();
+      });
+    });
+  });
 });
