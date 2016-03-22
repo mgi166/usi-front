12456c494c7b367553f0abc2e93a59c172af431f
mgi166 <skskoari@gmail.com>
2016-03-22 09:32:37 +0900

    Add King class inherits from Piece

---
+++ test/src/pieces/king_test.js
@@ -0,0 +1,8 @@
+import King from '../../../frontend/src/shogi/pieces/king';
+import memo from 'memo-is';
+import _ from 'lodash';
+
+describe('King', () => {
+  describe('#constructor', () => {
+  });
+});
