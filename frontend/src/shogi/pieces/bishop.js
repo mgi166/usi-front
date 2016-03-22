ba941631ed098d16dbc757552725db7a2f700a4b
mgi166 <skskoari@gmail.com>
2016-03-22 22:42:14 +0900

    Override Bishop constructor.

--- frontend/src/shogi/pieces/bishop.js
+++ frontend/src/shogi/pieces/bishop.js
@@ -2,4 +2,16 @@ import Piece from '../piece';
 import * as CONST from '../constants/pieceTypes';
 
 export default class Bishop extends Piece {
+  constructor({ type, x, y, movable = false, isPlaced =  false }) {
+    super({ type, x, y, movable, isPlaced });
+
+    if (this.type !== CONST.USI_BISHOP_BLACK_TYPE &&
+        this.type !== CONST.USI_BISHOP_WHITE_TYPE &&
+        this.type !== CONST.USI_BISHOP_BLACK_PROMOTE_TYPE &&
+        this.type !== CONST.USI_BISHOP_WHITE_PROMOTE_TYPE) {
+      throw new Error(`Unexpected type as Bishop: type is ${this.type}`);
+    }
+
+    return this;
+  }
 }
--- test/src/pieces/bishop_test.js
+++ test/src/pieces/bishop_test.js
@@ -3,4 +3,25 @@ import memo from 'memo-is';
 import _ from 'lodash';
 
 describe('Bishop', () => {
+  describe('#constructor', () => {
+    context('black', () => {
+      it('this.type is `B`', () => {
+        var bishop = new Bishop({ type: 'B' });
+        bishop.should.have.property('type', 'B');
+      });
+    });
+
+    context('white', () => {
+      it('this.type is `b`', () => {
+        var bishop = new Bishop({ type: 'b' });
+        bishop.should.have.property('type', 'b');
+      });
+    });
+
+    context('unknown type', () => {
+      it('throw exception', () => {
+        (() => { new Bishop({ type: '*' }); }).should.throw();
+      });
+    });
+  });
 });
