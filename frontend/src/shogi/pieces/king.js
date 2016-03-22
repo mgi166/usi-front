2042d0d91380890c3d61d410cfc49df6a8028fae
mgi166 <skskoari@gmail.com>
2016-03-22 09:35:27 +0900

    Override constructor of King

--- frontend/src/shogi/pieces/king.js
+++ frontend/src/shogi/pieces/king.js
@@ -2,4 +2,14 @@ import Piece from '../piece';
 import * as CONST from '../constants/pieceTypes';
 
 export default class King extends Piece {
+  constructor({ type, x, y, movable = false, isPlaced =  false }) {
+    super({ type, x, y, movable, isPlaced });
+
+    if (this.type !== CONST.USI_KING_BLACK_TYPE &&
+        this.type !== CONST.USI_KING_WHITE_TYPE) {
+      throw new Error(`Unexpected type as King: type is ${this.type}`);
+    }
+
+    return this;
+  }
 }
--- test/src/pieces/king_test.js
+++ test/src/pieces/king_test.js
@@ -4,5 +4,24 @@ import _ from 'lodash';
 
 describe('King', () => {
   describe('#constructor', () => {
+    context('black', () => {
+      it('this.type is `K`', () => {
+        var king = new King({ type: 'K' });
+        king.should.have.property('type', 'K');
+      });
+    });
+
+    context('white', () => {
+      it('this.type is `k`', () => {
+        var king = new King({ type: 'k' });
+        king.should.have.property('type', 'k');
+      });
+    });
+
+    context('unknown type', () => {
+      it('throw exception', () => {
+        (() => { new King({ type: '*' }); }).should.throw();
+      });
+    });
   });
 });
