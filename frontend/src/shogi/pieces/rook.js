e33a973fbe2fd8018a87cbc557f10fcc6f069942
mgi166 <skskoari@gmail.com>
2016-03-22 22:19:40 +0900

    Override constructor in Rook.

--- frontend/src/shogi/pieces/rook.js
+++ frontend/src/shogi/pieces/rook.js
@@ -2,4 +2,16 @@ import Piece from '../piece';
 import * as CONST from '../constants/pieceTypes';
 
 export default class Rook extends Piece {
+  constructor({ type, x, y, movable = false, isPlaced =  false }) {
+    super({ type, x, y, movable, isPlaced });
+
+    if (this.type !== CONST.USI_ROOK_BLACK_TYPE &&
+        this.type !== CONST.USI_ROOK_WHITE_TYPE &&
+        this.type !== CONST.USI_ROOK_BLACK_PROMOTE_TYPE &&
+        this.type !== CONST.USI_ROOK_WHITE_PROMOTE_TYPE) {
+      throw new Error(`Unexpected type as Rook: type is ${this.type}`);
+    }
+
+    return this;
+  }
 }
--- test/src/pieces/rook_test.js
+++ test/src/pieces/rook_test.js
@@ -3,4 +3,25 @@ import memo from 'memo-is';
 import _ from 'lodash';
 
 describe('Rook', () => {
+  describe('#constructor', () => {
+    context('black', () => {
+      it('this.type is `R`', () => {
+        var rook = new Rook({ type: 'R' });
+        rook.should.have.property('type', 'R');
+      });
+    });
+
+    context('white', () => {
+      it('this.type is `r`', () => {
+        var rook = new Rook({ type: 'r' });
+        rook.should.have.property('type', 'r');
+      });
+    });
+
+    context('unknown type', () => {
+      it('throw exception', () => {
+        (() => { new Rook({ type: '*' }); }).should.throw();
+      });
+    });
+  });
 });
