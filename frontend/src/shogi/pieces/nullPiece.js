9d0d19fdf1e575186d97fb0d8d6f04eddfe2b9a6
mgi166 <skskoari@gmail.com>
2016-03-22 23:03:30 +0900

    Override constructor in NullPiece.

--- frontend/src/shogi/pieces/nullPiece.js
+++ frontend/src/shogi/pieces/nullPiece.js
@@ -2,4 +2,9 @@ import Piece from '../piece';
 import * as CONST from '../constants/pieceTypes';
 
 export default class NullPiece extends Piece {
+  constructor({ type, x, y, movable = false, isPlaced =  false }) {
+    super({ type, x, y, movable, isPlaced });
+    this.type = CONST.USI_NULL_TYPE;
+    return this;
+  }
 }
--- test/src/pieces/nullPiece_test.js
+++ test/src/pieces/nullPiece_test.js
@@ -3,4 +3,10 @@ import memo from 'memo-is';
 import _ from 'lodash';
 
 describe('NullPiece', () => {
+  describe('#constructor', () => {
+    it('this.type is `*`', () => {
+      var piece = new NullPiece({ type: '*' });
+      piece.type.should.eql('*');
+    });
+  });
 });
