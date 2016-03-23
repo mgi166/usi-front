dde23851ac004d289697b7ee08e34283a35c61dc
mgi166 <skskoari@gmail.com>
2016-03-23 23:31:51 +0900

    [wip] Add pieces index.js

---
+++ frontend/src/shogi/pieces/index.js
@@ -0,0 +1,11 @@
+import Bishop from './bishop';
+import Gold from './gold';
+import King from './king';
+import Knight from './knight';
+import Lance from './lance';
+import NullPiece from './nullPiece';
+import Pawn from './pawn';
+import Rook from './rook';
+import Silver from './silver';
+
+export { Bishop, Gold, King, Knight, Lance, NullPiece, Pawn, Rook, Silver }
