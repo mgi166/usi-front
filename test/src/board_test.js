66aa0561a8aef249b84bcd259c9933e73f83e845
mgi166 <skskoari@gmail.com>
2016-03-27 00:52:31 +0900

    Add #movePiece test.

--- test/src/board_test.js
+++ test/src/board_test.js
@@ -1,6 +1,7 @@
 import Board from '../../frontend/src/shogi/board';
 import Piece from '../../frontend/src/shogi/piece';
 import Turn from '../../frontend/src/shogi/turn';
+import * as CONST from '../../frontend/src/shogi/constants/boardTypes';
 import memo from 'memo-is';
 import _ from 'lodash';
 
@@ -66,5 +67,60 @@ describe('#invertCor', () => {
     board().board[y][x].type.should.eql('*');
     board().board[y][x].x.should.eql(3);
     board().board[y][x].y.should.eql(6);
+  });
+});
+
+describe('#movePiece', () => {
+  var board = memo().is(() => {
+    var _board = new Board();
+    _board.setBoard(position());
+    return(_board);
+  });
+
+  var position = memo().is(() => { return CONST.USI_INITIAL_BOARD; });
+
+  context('once', () => {
+    it('moved piece', () => {
+      var fromPiece = Piece.create({ type: 'P', x: 2, y: 7 });
+      var toPiece = Piece.create({ type: 'P', x: 2, y: 6 });
+
+      var _board = board().movePiece(fromPiece, toPiece);
+      new Board(_board).toArray().should.eql(
+        [
+          [ 'l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l' ],
+          [ '*', 'b', '*', '*', '*', '*', '*', 'r', '*' ],
+          [ 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' ],
+          [ '*', '*', '*', '*', '*', '*', '*', '*', '*' ],
+          [ '*', '*', '*', '*', '*', '*', '*', '*', '*' ],
+          [ '*', '*', '*', '*', '*', '*', '*', 'P', '*' ],
+          [ 'P', 'P', 'P', 'P', 'P', 'P', 'P', '*', 'P' ],
+          [ '*', 'B', '*', '*', '*', '*', '*', 'R', '*' ],
+          [ 'L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L' ]
+        ]
+      );
+    });
+
+    // NOTE: if this.board is changed, reducer does not recognize state changing.
+    //       Therefore, this.board does not change after #movePiece.
+    it('does not change this.board', () => {
+      var fromPiece = Piece.create({ type: 'P', x: 2, y: 7 });
+      var toPiece = Piece.create({ type: 'P', x: 2, y: 6 });
+
+      board().movePiece(fromPiece, toPiece);
+
+      board().toArray().should.eql(
+        [
+          [ 'l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l' ],
+          [ '*', 'b', '*', '*', '*', '*', '*', 'r', '*' ],
+          [ 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' ],
+          [ '*', '*', '*', '*', '*', '*', '*', '*', '*' ],
+          [ '*', '*', '*', '*', '*', '*', '*', '*', '*' ],
+          [ '*', '*', '*', '*', '*', '*', '*', '*', '*' ],
+          [ 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' ],
+          [ '*', 'B', '*', '*', '*', '*', '*', 'R', '*' ],
+          [ 'L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L' ]
+        ]
+      );
+    });
   });
 });
