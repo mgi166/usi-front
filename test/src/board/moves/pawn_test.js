0159e63c437f67fd465dfc3bc72caaa77c8d9940
mgi166 <skskoari@gmail.com>
2016-04-23 23:26:19 +0900

    Nest spec of #enhanceMovablePoint for Pawn.

--- test/src/board/moves/pawn_test.js
+++ test/src/board/moves/pawn_test.js
@@ -3,9 +3,75 @@ import Piece from '../../../../frontend/src/shogi/piece';
 import memo from 'memo-is';
 import _ from 'lodash';
 
-describe('black', () => {
-  context('match the piece to coordinate', () => {
-    context('exists movable coordinates', () => {
+describe('#enhanceMovablePoint', () => {
+  describe('black', () => {
+    context('match the piece to coordinate', () => {
+      context('exists movable coordinates', () => {
+        var board = memo().is(() => {
+          var _board = new Board;
+          _board.setBoard(position());
+          return(_board);
+        });
+
+        var position = memo().is(() => {
+          return (
+            [
+              ['*', '*', '*'],
+              ['*', 'P', '*']
+            ]
+          );
+        });
+
+        it('change property of piece that is moved', () => {
+          var piece = Piece.create({ type: 'P', x: 8, y: 2 });
+
+          board().enhanceMovablePoint(piece);
+
+          var movablePieces = board().board.map((row) => {
+            return (
+              row.filter((cell) => { return(cell.movable); })
+            );
+          });
+
+          _.flattenDeep(movablePieces).should.eql(
+            [Piece.create({ type: '*', x: 8, y: 1, movable: true })]
+          );
+        });
+      });
+
+      context('does not exist movable coordinates', () => {
+        var board = memo().is(() => {
+          var _board = new Board;
+          _board.setBoard(position());
+          return(_board);
+        });
+
+        var position = memo().is(() => {
+          return (
+            [
+              ['P', '*', '*'],
+              ['*', '*', '*']
+            ]
+          );
+        });
+
+        it('does not change property of piece', () => {
+          var piece = Piece.create({ type: 'P', x: 9, y: 1 });
+
+          board().enhanceMovablePoint(piece);
+
+          var movablePieces = board().board.map((row) => {
+            return (
+              row.filter((cell) => { return(cell.movable); })
+            );
+          });
+
+          _.flattenDeep(movablePieces).should.eql([]);
+        });
+      });
+    });
+
+    context('mismatch the piece to coordinate', () => {
       var board = memo().is(() => {
         var _board = new Board;
         _board.setBoard(position());
@@ -16,29 +82,90 @@ describe('black', () => {
         return (
           [
             ['*', '*', '*'],
-            ['*', 'P', '*']
+            ['*', '*', 'P']
           ]
         );
       });
 
-      it('change property of piece that is moved', () => {
+      it('throw exception', () => {
         var piece = Piece.create({ type: 'P', x: 8, y: 2 });
 
-        board().enhanceMovablePoint(piece);
-
-        var movablePieces = board().board.map((row) => {
-          return (
-            row.filter((cell) => { return(cell.movable); })
-          );
-        });
-
-        _.flattenDeep(movablePieces).should.eql(
-          [Piece.create({ type: '*', x: 8, y: 1, movable: true })]
-        );
+        (() => { return board().enhanceMovablePoint(piece); }).should.throw();;
       });
     });
 
-    context('does not exist movable coordinates', () => {
+    context.skip('if move piece, king is taken', () => {
+    });
+  });
+
+  describe('white', () => {
+    context('match the piece to coordinate', () => {
+      context('exists movable coordinates', () => {
+        var board = memo().is(() => {
+          var _board = new Board;
+          _board.setBoard(position());
+          return(_board);
+        });
+
+        var position = memo().is(() => {
+          return (
+            [
+              ['*', 'p', '*'],
+              ['*', '*', '*']
+            ]
+          );
+        });
+
+        it('change property of piece that is moved', () => {
+          var piece = Piece.create({ type: 'p', x: 8, y: 1 });
+
+          board().enhanceMovablePoint(piece);
+
+          var movablePieces = board().board.map((row) => {
+            return (
+              row.filter((cell) => { return(cell.movable); })
+            );
+          });
+
+          _.flattenDeep(movablePieces).should.eql(
+            [Piece.create({ type: '*', x: 8, y: 2, movable: true })]
+          );
+        });
+      });
+
+      context('does not exist movable coordinates', () => {
+        var board = memo().is(() => {
+          var _board = new Board;
+          _board.setBoard(position());
+          return(_board);
+        });
+
+        var position = memo().is(() => {
+          return (
+            [
+              ['*', '*', '*'],
+              ['p', '*', '*']
+            ]
+          );
+        });
+
+        it('does not change property of piece', () => {
+          var piece = Piece.create({ type: 'p', x: 9, y: 2 });
+
+          board().enhanceMovablePoint(piece);
+
+          var movablePieces = board().board.map((row) => {
+            return (
+              row.filter((cell) => { return(cell.movable); })
+            );
+          });
+
+          _.flattenDeep(movablePieces).should.eql([]);
+        });
+      });
+    });
+
+    context('mismatch the piece to coordinate', () => {
       var board = memo().is(() => {
         var _board = new Board;
         _board.setBoard(position());
@@ -48,146 +175,21 @@ describe('black', () => {
       var position = memo().is(() => {
         return (
           [
-            ['P', '*', '*'],
+            ['*', '*', 'p'],
             ['*', '*', '*']
           ]
         );
       });
 
-      it('does not change property of piece', () => {
-        var piece = Piece.create({ type: 'P', x: 9, y: 1 });
+      it('throw exception', () => {
+        var piece = Piece.create({ type: 'p', x: 9, y: 1 });
 
-        board().enhanceMovablePoint(piece);
-
-        var movablePieces = board().board.map((row) => {
-          return (
-            row.filter((cell) => { return(cell.movable); })
-          );
-        });
-
-        _.flattenDeep(movablePieces).should.eql([]);
-      });
-    });
-  });
-
-  context('mismatch the piece to coordinate', () => {
-    var board = memo().is(() => {
-      var _board = new Board;
-      _board.setBoard(position());
-      return(_board);
-    });
-
-    var position = memo().is(() => {
-      return (
-        [
-          ['*', '*', '*'],
-          ['*', '*', 'P']
-        ]
-      );
-    });
-
-    it('throw exception', () => {
-      var piece = Piece.create({ type: 'P', x: 8, y: 2 });
-
-      (() => { return board().enhanceMovablePoint(piece); }).should.throw();;
-    });
-  });
-
-  context.skip('if move piece, king is taken', () => {
-  });
-});
-
-describe('white', () => {
-  context('match the piece to coordinate', () => {
-    context('exists movable coordinates', () => {
-      var board = memo().is(() => {
-        var _board = new Board;
-        _board.setBoard(position());
-        return(_board);
-      });
-
-      var position = memo().is(() => {
-        return (
-          [
-            ['*', 'p', '*'],
-            ['*', '*', '*']
-          ]
-        );
-      });
-
-      it('change property of piece that is moved', () => {
-        var piece = Piece.create({ type: 'p', x: 8, y: 1 });
-
-        board().enhanceMovablePoint(piece);
-
-        var movablePieces = board().board.map((row) => {
-          return (
-            row.filter((cell) => { return(cell.movable); })
-          );
-        });
-
-        _.flattenDeep(movablePieces).should.eql(
-          [Piece.create({ type: '*', x: 8, y: 2, movable: true })]
-        );
+        (() => { return board().enhanceMovablePoint(piece); }).should.throw();;
       });
     });
 
-    context('does not exist movable coordinates', () => {
-      var board = memo().is(() => {
-        var _board = new Board;
-        _board.setBoard(position());
-        return(_board);
-      });
-
-      var position = memo().is(() => {
-        return (
-          [
-            ['*', '*', '*'],
-            ['p', '*', '*']
-          ]
-        );
-      });
-
-      it('does not change property of piece', () => {
-        var piece = Piece.create({ type: 'p', x: 9, y: 2 });
-
-        board().enhanceMovablePoint(piece);
-
-        var movablePieces = board().board.map((row) => {
-          return (
-            row.filter((cell) => { return(cell.movable); })
-          );
-        });
-
-        _.flattenDeep(movablePieces).should.eql([]);
-      });
+    context.skip('if move piece, king is taken', () => {
     });
-  });
-
-  context('mismatch the piece to coordinate', () => {
-    var board = memo().is(() => {
-      var _board = new Board;
-      _board.setBoard(position());
-      return(_board);
-    });
-
-    var position = memo().is(() => {
-      return (
-        [
-          ['*', '*', 'p'],
-          ['*', '*', '*']
-        ]
-      );
-    });
-
-    it('throw exception', () => {
-      var piece = Piece.create({ type: 'p', x: 9, y: 1 });
-
-      (() => { return board().enhanceMovablePoint(piece); }).should.throw();;
-    });
-  });
-
-  context.skip('if move piece, king is taken', () => {
   });
 });
 
