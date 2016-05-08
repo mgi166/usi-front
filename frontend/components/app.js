b86fb6efe8b6562a3b843a8e7c7e917ac2e3eecf
mgi166 <skskoari@gmail.com>
2016-05-08 14:14:29 +0900

    Add App component.
    * Load application root component.

---
+++ frontend/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from './appBar';
+import Game from './game';
+
+const App = () => {
+  return (
+    <div>
+      <AppBar />
+      <h1>hello</h1>
+      <Game />
+    </div>
+  );
+};
+
+export default App;
