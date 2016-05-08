7e7ae1ba4a09411ef589e056700e27fd63017a5f
mgi166 <skskoari@gmail.com>
2016-05-08 15:01:59 +0900

    Add Container component.

---
+++ frontend/components/container.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppBar from './appBar';
+import App from './app';
+import Footer from './footer';
+
+const Container = () => {
+  return (
+    <div>
+      <AppBar />
+      <App />
+      <Footer />
+    </div>
+  );
+};
+
+export default Container;
