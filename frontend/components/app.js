4bd6ad6db66bfa4738f071cd7c9b8f30aacf7214
mgi166 <skskoari@gmail.com>
2016-05-08 15:02:25 +0900

    Remove AppBar, Footer.
    * Move to container component.

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import AppBar from './appBar';
 import Game from './game';
-import Footer from './footer';
 
 const App = () => {
   return (
     <div>
-      <AppBar />
       <h1>hello</h1>
       <Game />
-      <Footer />
     </div>
   );
 };
