caeb3c78a6d0218dc50e2d0f6fee463a135f143c
mgi166 <skskoari@gmail.com>
2016-06-04 18:02:51 +0900

    Add PieceStand#add method.
    * Add piece, and save piece type the number.

--- frontend/src/shogi/pieceStand.js
+++ frontend/src/shogi/pieceStand.js
@@ -1,4 +1,18 @@
 export default class PieceStand {
   constructor () {
+    this.pieceTypes = {};
+    this.pieces = new Set;
+  }
+
+  add(piece) {
+    // TODO: Refactor.
+    if (this.pieceTypes[piece.type]) {
+      this.pieceTypes[piece.type] ++;
+    } else {
+      this.pieceTypes[piece.type] = 1;
+    }
+
+    this.pieces.add(piece);
+    return piece;
   }
 }
---
+++ test/src/pieceStand_test.js
@@ -0,0 +1,39 @@
+import PieceStand from '../../frontend/src/shogi/pieceStand';
+import Piece from '../../frontend/src/shogi/piece';
+import memo from 'memo-is';
+import _ from 'lodash';
+
+describe('PieceStand', () => {
+  describe('#add', () => {
+    context('when add new type piece', () => {
+      it('return piece', () => {
+        const pieceStand = new PieceStand;
+        const piece = Piece.create({ type: 'p' });
+
+        pieceStand.add(piece).should.eql(piece);
+      });
+
+      it('store the added piece in piece stand', () => {
+        const pieceStand = new PieceStand;
+        const piece1 = Piece.create({ type: 'p' });
+        const piece2 = Piece.create({ type: 'p' });
+        const piece3 = Piece.create({ type: 'l' });
+        const piece4 = Piece.create({ type: 'n' });
+
+        pieceStand.add(piece1);
+        pieceStand.add(piece2);
+        pieceStand.add(piece3);
+        pieceStand.add(piece4);
+
+        pieceStand.pieces.size.should.eql(4);
+        pieceStand.pieceTypes.should.eql(
+          {
+            p: 2,
+            l: 1,
+            n: 1
+          }
+        );
+      });
+    });
+  });
+});
