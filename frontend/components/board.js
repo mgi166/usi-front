b74a69d75fd191d226a830a0a77b460a4c782da1
mgi166 <skskoari@gmail.com>
2016-06-05 11:49:42 +0900

    No use React.Component to separate dependency from component.

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -8,29 +8,27 @@ const mapStateToProps = (state) => {
   return { board: state.board.board };
 };
 
-class ShogiBoard extends React.Component {
-  render() {
-    var tbody = this.props.board.map((row, y) => {
-      var rows = row.map((piece, x) => {
-        return (
+const ShogiBoard = ({ board }) => {
+  const tbody = board.map((row, y) => {
+    const rows = row.map((piece, x) => {
+      return (
           <Piece key={x} piece={piece} />
-        );
-      });
-      return(<tr key={y}>{rows}</tr>);
+      );
     });
+    return(<tr key={y}>{rows}</tr>);
+  });
 
-    return(
-      <Grid>
-        <Row>
-          <Col xs={12} md={4}>
-            <table className="board">
-              <tbody>{tbody}</tbody>
-            </table>
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
+  return (
+    <Grid>
+      <Row>
+        <Col xs={12} md={4}>
+          <table className="board">
+            <tbody>{tbody}</tbody>
+          </table>
+        </Col>
+      </Row>
+    </Grid>
+  );
 };
 
 const Board = connect(
