128ca2e527f61685b0fcff76994a965d3c18b61a
mgi166 <skskoari@gmail.com>
2016-06-05 11:27:56 +0900

    Add `blackPieceStand`, `whitePieceStand` keys to store.
    * Take the piece, store the piece to pieceStand.

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -3,7 +3,9 @@ import * as CONST from '../constants/actionTypes';
 
 const InitialState = {
   board: Shogi.Board,
-  isHoldingPiece: undefined
+  isHoldingPiece: undefined,
+  blackPieceStand: [],
+  whitePieceStand: []
 };
 
 const ShogiReducer = (state = InitialState, action) => {
@@ -13,14 +15,56 @@ const ShogiReducer = (state = InitialState, action) => {
       return state;
     } else {
       let newBoard = state.board.enhanceMovablePoint(action.piece);
-      return Object.assign({}, { board: newBoard, isHoldingPiece: action.piece });
+      return Object.assign(
+        {},
+        {
+          board: newBoard,
+          blackPieceStand: state.blackPieceStand,
+          whitePieceStand: state.whitePieceStand,
+          isHoldingPiece: action.piece
+        }
+      );
     }
   case CONST.MOVE_PIECE:
     // if same piece click, release piece.
     if (state.isHoldingPiece) {
       let newBoard = state.board.movePiece(state.isHoldingPiece, action.piece);
 
-      return Object.assign({}, { board: newBoard }, { isHoldingPiece: undefined });
+      if (newBoard.takedPiece) {
+        // TODO: Use constants. Refactor.
+        switch (newBoard.takedPiece.team()) {
+        case 'black':
+          state.blackPieceStand.push(newBoard.takedPiece);
+          return Object.assign(
+            {},
+            { board: newBoard, whitePieceStand: state.whitePieceStand },
+            {
+              isHoldingPiece: undefined,
+              blackPieceStand: state.blackPieceStand
+            }
+          );
+        case 'white':
+          state.whitePieceStand.push(newBoard.takedPiece);
+          return Object.assign(
+            {},
+            { board: newBoard, blackPieceStand: state.blackPieceStand },
+            {
+              isHoldingPiece: undefined,
+              whitePieceStand: state.whitePieceStand
+            }
+          );
+        }
+      } else {
+        return Object.assign(
+          {},
+          {
+            board: newBoard,
+            blackPieceStand: state.blackPieceStand,
+            whitePieceStand: state.whitePieceStand
+          },
+          { isHoldingPiece: undefined }
+        );
+      }
     } else {
       return Object.assign({}, state, { isHoldingPiece: action.piece });
     }
