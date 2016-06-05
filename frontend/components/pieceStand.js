eedbc901580fadb1e1d77b7ab4dc1a29b7fa1acf
mgi166 <skskoari@gmail.com>
2016-06-05 12:39:08 +0900

    PieceStand component receives pieceStand. and render it.

--- frontend/components/pieceStand.js
+++ frontend/components/pieceStand.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { getPieceImage } from '../images/shogiPieces/index';
 
-const PieceStand = ({}) => {
+const PieceStand = ({ pieceStand }) => {
+  const tr = pieceStand.map((piece) => {
+    return (
+      <tr>
+        <td>
+          <img src={getPieceImage(piece)}></img>
+        </td>
+      </tr>
+    );
+  });
+
   return (
     <div>
       <table>
         <tbody>
-          <tr>
-            <td>
-              aaaa
-            </td>
-          </tr>
+          {tr}
         </tbody>
       </table>
     </div>
