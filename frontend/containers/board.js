2d30d1902922ab94ff8a69bed8f971e7237dd3a1
mgi166 <skskoari@gmail.com>
2016-06-14 21:42:02 +0900

    Dispatch `hidePromoteModal` action in board component.

--- frontend/containers/board.js
+++ frontend/containers/board.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import board from '../components/board';
+import { hidePromoteModal } from '../actions';
 
 const mapStateToProps = (state) => {
   return { board: state.board.board, promoteModal: state.promoteModal };
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onHidePromoteModal: () => { dispatch(hidePromoteModal()); }
+  };
+};
+
 const Board = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(board);
 
 export default Board;
