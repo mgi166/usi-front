3481c53c20554b505ed07505d8498677b9d4bf69
mgi166 <skskoari@gmail.com>
2016-06-14 23:39:28 +0900

    sq. initial state.

--- frontend/reducers/promoteModal.js
+++ frontend/reducers/promoteModal.js
@@ -2,7 +2,7 @@ import * as CONST from '../constants/actionTypes';
 
 const initialState = { open: false };
 
-export default function promoteModal(state, action) {
+export default function promoteModal(state = initialState, action) {
   switch (action.type) {
   case CONST.SHOW_PROMOTE_MODAL:
     return { open: true };
