c12b660b8d4b5b31a3362341fed996cfa450c614
mgi166 <skskoari@gmail.com>
2016-06-14 22:27:35 +0900

    Add `promoteModal` reducer. Extract from reducers/index.js

---
+++ frontend/reducers/promoteModal.js
@@ -0,0 +1,14 @@
+import * as CONST from '../constants/actionTypes';
+
+const initialState = { open: false };
+
+export default function promoteModal(state, action) {
+  switch (action.type) {
+  case CONST.SHOW_PROMOTE_MODAL:
+    return { open: true };
+  case CONST.HIDE_PROMOTE_MODAL:
+    return { open: false };
+  default:
+    return state;
+  }
+}
