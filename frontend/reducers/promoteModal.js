fc81c225801101221d308f300d2491a39d2db22e
mgi166 <skskoari@gmail.com>
2016-06-18 18:12:23 +0900

    Update reducer properties, allow `piece` key.
    * the `piece` is the target promote or not.

--- frontend/reducers/promoteModal.js
+++ frontend/reducers/promoteModal.js
@@ -1,13 +1,13 @@
 import * as CONST from '../constants/actionTypes';
 
-const initialState = { open: false };
+const initialState = { open: false, piece: undefined };
 
 export default function promoteModal(state = initialState, action) {
   switch (action.type) {
   case CONST.SHOW_PROMOTE_MODAL:
-    return { open: true };
+    return { open: true, piece: action.piece };
   case CONST.HIDE_PROMOTE_MODAL:
-    return { open: false };
+    return { open: false, piece: undefined };
   default:
     return state;
   }
