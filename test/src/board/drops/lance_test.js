79317272a29289b66eb209815a40f1e9e03e38c7
mgi166 <skskoari@gmail.com>
2016-06-20 22:51:46 +0900

    Refactor. Surround another describe.

--- test/src/board/drops/lance_test.js
+++ test/src/board/drops/lance_test.js
@@ -3,91 +3,93 @@ import Piece from '../../../../frontend/src/shogi/piece';
 import memo from 'memo-is';
 import _ from 'lodash';
 
-describe('#enhancePlaceablePoint', () => {
-  describe('black', () => {
-    context('the normal pattern', () => {
-      const position = memo().is(() => {
-        return (
-          [
-            ['*', '*', '*'],
-            ['R', '*', '*'],
-            ['*', '*', 'p'],
-            ['*', '*', '*']
-          ]
-        );
-      });
-
-      it('change property of piece that is placeable', () => {
-        const board = new Board(position());
-        const piece = Piece.create({ type: 'L', x: 0, y: 0});
-
-        const placeablePieces = board.enhancePlaceablePoint(piece).board.map((row) => {
+describe('Lance', () => {
+  describe('#enhancePlaceablePoint', () => {
+    describe('black', () => {
+      context('the normal pattern', () => {
+        const position = memo().is(() => {
           return (
-            row.filter((cell) => { return(cell.isPlaced); })
+            [
+              ['*', '*', '*'],
+              ['R', '*', '*'],
+              ['*', '*', 'p'],
+              ['*', '*', '*']
+            ]
           );
         });
 
-        _.flattenDeep(placeablePieces).should.eql(
-          [
-            Piece.create({ type: '*', x: 8, y: 2, isPlaced: true}),
-            Piece.create({ type: '*', x: 7, y: 2, isPlaced: true}),
-            Piece.create({ type: '*', x: 9, y: 3, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 3, isPlaced: true}),
-            Piece.create({ type: '*', x: 9, y: 4, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 4, isPlaced: true}),
-            Piece.create({ type: '*', x: 7, y: 4, isPlaced: true}),
-          ]
-        );
+        it('change property of piece that is placeable', () => {
+          const board = new Board(position());
+          const piece = Piece.create({ type: 'L', x: 0, y: 0});
+
+          const placeablePieces = board.enhancePlaceablePoint(piece).board.map((row) => {
+            return (
+              row.filter((cell) => { return(cell.isPlaced); })
+            );
+          });
+
+          _.flattenDeep(placeablePieces).should.eql(
+            [
+              Piece.create({ type: '*', x: 8, y: 2, isPlaced: true}),
+              Piece.create({ type: '*', x: 7, y: 2, isPlaced: true}),
+              Piece.create({ type: '*', x: 9, y: 3, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 3, isPlaced: true}),
+              Piece.create({ type: '*', x: 9, y: 4, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 4, isPlaced: true}),
+              Piece.create({ type: '*', x: 7, y: 4, isPlaced: true}),
+            ]
+          );
+        });
       });
     });
-  });
 
-  describe('white', () => {
-    context('the normal pattern', () => {
-      const position = memo().is(() => {
-        return (
-          [
-            ['*', '*'],
-            ['*', '*'],
-            ['*', '*'],
-            ['*', 'P'],
-            ['*', '*'],
-            ['*', '*'],
-            ['b', '*'],
-            ['*', '*'],
-            ['*', '*'],
-          ]
-        );
-      });
-
-      it('change property of piece that is placeable', () => {
-        const board = new Board(position());
-        const piece = Piece.create({ type: 'l', x: 0, y: 0});
-
-        const placeablePieces = board.enhancePlaceablePoint(piece).board.map((row) => {
+    describe('white', () => {
+      context('the normal pattern', () => {
+        const position = memo().is(() => {
           return (
-            row.filter((cell) => { return(cell.isPlaced); })
+            [
+              ['*', '*'],
+              ['*', '*'],
+              ['*', '*'],
+              ['*', 'P'],
+              ['*', '*'],
+              ['*', '*'],
+              ['b', '*'],
+              ['*', '*'],
+              ['*', '*'],
+            ]
           );
         });
 
-        _.flattenDeep(placeablePieces).should.eql(
-          [
-            Piece.create({ type: '*', x: 9, y: 1, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 1, isPlaced: true}),
-            Piece.create({ type: '*', x: 9, y: 2, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 2, isPlaced: true}),
-            Piece.create({ type: '*', x: 9, y: 3, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 3, isPlaced: true}),
-            Piece.create({ type: '*', x: 9, y: 4, isPlaced: true}),
-            Piece.create({ type: '*', x: 9, y: 5, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 5, isPlaced: true}),
-            Piece.create({ type: '*', x: 9, y: 6, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 6, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 7, isPlaced: true}),
-            Piece.create({ type: '*', x: 9, y: 8, isPlaced: true}),
-            Piece.create({ type: '*', x: 8, y: 8, isPlaced: true}),
-          ]
-        );
+        it('change property of piece that is placeable', () => {
+          const board = new Board(position());
+          const piece = Piece.create({ type: 'l', x: 0, y: 0});
+
+          const placeablePieces = board.enhancePlaceablePoint(piece).board.map((row) => {
+            return (
+              row.filter((cell) => { return(cell.isPlaced); })
+            );
+          });
+
+          _.flattenDeep(placeablePieces).should.eql(
+            [
+              Piece.create({ type: '*', x: 9, y: 1, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 1, isPlaced: true}),
+              Piece.create({ type: '*', x: 9, y: 2, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 2, isPlaced: true}),
+              Piece.create({ type: '*', x: 9, y: 3, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 3, isPlaced: true}),
+              Piece.create({ type: '*', x: 9, y: 4, isPlaced: true}),
+              Piece.create({ type: '*', x: 9, y: 5, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 5, isPlaced: true}),
+              Piece.create({ type: '*', x: 9, y: 6, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 6, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 7, isPlaced: true}),
+              Piece.create({ type: '*', x: 9, y: 8, isPlaced: true}),
+              Piece.create({ type: '*', x: 8, y: 8, isPlaced: true}),
+            ]
+          );
+        });
       });
     });
   });
