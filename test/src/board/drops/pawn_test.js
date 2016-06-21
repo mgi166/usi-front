c1fa8571c27a192706011b9138e552e60367857c
mgi166 <skskoari@gmail.com>
2016-06-21 22:30:09 +0900

    Add dropPiece method for drop the holding piece.

--- test/src/board/drops/pawn_test.js
+++ test/src/board/drops/pawn_test.js
@@ -90,4 +90,61 @@ describe('Pawn', () => {
       });
     });
   });
+
+  describe('#dropPiece', () => {
+    context('black', () => {
+      const position = memo().is(() => {
+        return [
+          ['*', '*', '*'],
+          ['P', '*', 'p'],
+          ['*', '*', '*'],
+        ];
+      });
+
+      context('when specify the piece that destination is able to drop', () => {
+        it('drop holding piece to destination piece', () => {
+          const board = new Board(position());
+
+          const holdingPiece = Piece.create({ type: 'P' });
+          const destPiece = Piece.create({ type: '*', x: 8, y: 2 });
+
+          board.dropPiece(holdingPiece, destPiece).toArray().should.eql(
+            [
+              ['*', '*', '*'],
+              ['P', 'P', 'p'],
+              ['*', '*', '*'],
+            ]
+          );
+        });
+      });
+
+      context('when specify the piece that destination is not able to drop', () => {
+        it('do nothing', () => {
+          const board = new Board(position());
+
+          const holdingPiece = Piece.create({ type: 'P' });
+          const destPiece = Piece.create({ type: '*', x: 9, y: 3 });
+
+          board.dropPiece(holdingPiece, destPiece).toArray().should.eql(
+            [
+              ['*', '*', '*'],
+              ['P', '*', 'p'],
+              ['*', '*', '*'],
+            ]
+          );
+        });
+      });
+
+      context('when specify the piece that does not found', () => {
+        it('throw error', () => {
+          const board = new Board(position());
+
+          const holdingPiece = Piece.create({ type: 'P' });
+          const destPiece = Piece.create({ type: 'G', x: 7, y: 3 });
+
+          (() => { board.dropPiece(holdingPiece, destPiece); }).should.throw();
+        });
+      });
+    });
+  });
 });
