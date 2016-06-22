3b68d5e20f8ed238c5d34c069f1da3677b9a05cc
mgi166 <skskoari@gmail.com>
2016-06-22 09:38:55 +0900

    Receive `dropped` attributes.

--- frontend/src/shogi/piece.js
+++ frontend/src/shogi/piece.js
@@ -3,16 +3,17 @@ import * as CONST from './constants/pieceTypes';
 import { getClass } from './pieces/index';
 
 export default class Piece {
-  constructor({ type, x, y, movable = false, isDrop =  false }) {
+  constructor({ type, x, y, movable = false, isDrop =  false, dropped = false }) {
     this.type = type;
     this.movable = movable;
     this.isDrop = isDrop;
+    this.dropped = dropped;
     this.x = x;
     this.y = y;
   }
 
-  static create({ type, x, y, movable, isDrop }) {
+  static create({ type, x, y, movable, isDrop, dropped }) {
     var klass = getClass(type);
-    return new klass({ type: type, x: x, y: y, movable: movable, isDrop: isDrop });
+    return new klass({ type: type, x: x, y: y, movable: movable, isDrop: isDrop, dropped: dropped });
   }
 };
