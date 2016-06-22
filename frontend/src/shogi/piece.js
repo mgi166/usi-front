4a14b2f64f23a7486320f7ea19853a2783ffbab1
mgi166 <skskoari@gmail.com>
2016-06-22 09:47:59 +0900

    Remove unnecessary constants.

--- frontend/src/shogi/piece.js
+++ frontend/src/shogi/piece.js
@@ -3,15 +3,6 @@ import * as CONST from './constants/pieceTypes';
 import { getClass } from './pieces/index';
 
 export default class Piece {
-  constructor({ type, x, y, movable = false, isDrop =  false, dropped = false }) {
-    this.type = type;
-    this.movable = movable;
-    this.isDrop = isDrop;
-    this.dropped = dropped;
-    this.x = x;
-    this.y = y;
-  }
-
   static create({ type, x, y, movable, isDrop, dropped }) {
     var klass = getClass(type);
     return new klass({ type: type, x: x, y: y, movable: movable, isDrop: isDrop, dropped: dropped });
