7dda638d52cb19cd8114aa36ae59473aa822af53
mgi166 <skskoari@gmail.com>
2016-06-22 23:02:21 +0900

    Remove unuse style.

--- frontend/components/piece.js
+++ frontend/components/piece.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { getPieceImage } from '../images/shogiPieces/index';
 
 const Piece = ({ piece, onPieceClick }) => {
-  const style = {
-    backgroundColor: piece.movable ? "red" : "white"
-  };
-
   return (
     <div className="piece" onClick={() => onPieceClick(piece)}>
       <img src={getPieceImage(piece)}></img>
