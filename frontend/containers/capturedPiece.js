57540a43d98c42dfabc8e5c579d7bb59f6bb211f
mgi166 <skskoari@gmail.com>
2016-06-23 09:53:43 +0900

    If does not holding piece, then hold the piece, and enhance the board.

--- frontend/containers/capturedPiece.js
+++ frontend/containers/capturedPiece.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import pieceComponent from '../components/piece';
+import { holdPiece, enhanceCanDropPosition } from '../actions';
 import store from '../stores/index';
 
 const mapStateToProps = (state) => {
@@ -11,6 +12,17 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     onPieceClick: (piece) => {
+      const state = store.getState();
+      const board = state.shogi.board();
+
+      const holdingPiece = state.shogi.holdingPiece;
+
+      if (!holdingPiece) {
+        // FIX: holdingPiece equals piece. It's confusing.
+        dispatch(holdPiece(piece));
+        dispatch(enhanceCanDropPosition(piece));
+        return;
+      }
     }
   };
 };
