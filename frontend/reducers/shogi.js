7e1ef78647692210343999ab27afd94074f11b59
mgi166 <skskoari@gmail.com>
2016-06-23 09:53:12 +0900

    Add `enhanceCanDropPosition` case.

--- frontend/reducers/shogi.js
+++ frontend/reducers/shogi.js
@@ -28,6 +28,8 @@ export default function shogi(state = initialState, action) {
     return { ...state, whitePieceStand: state.whitePieceStand.concat([action.piece.toOpponentPiece()]) };
   case CONST.PROMOTE_PIECE:
     return { ...state, board: state.board.promotePiece(action.piece) };
+  case CONST.ENHANCE_CAN_DROP_POSITION:
+    return { ...state, board: state.board.enhanceCanDropPosition(action.piece) };
   default:
     return state;
   }
