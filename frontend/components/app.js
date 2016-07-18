bf10bef5ea6c1787b4a539a611a3429bd15e3773
mgi166 <skskoari@gmail.com>
2016-07-18 10:58:00 +0900

    Move components to App components.
    * For separate board compoennts.

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Board from '../containers/board';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import BlackPieceStand from '../containers/blackPieceStand';
+import WhitePieceStand from '../containers/whitePieceStand';
 
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
-      <Board />
+      <Grid>
+        <Row center="xs">
+          <Col xs={1}>
+            <WhitePieceStand />
+          </Col>
+          <Col xs={6}>
+            <Board />
+          </Col>
+          <Col xs={1}>
+            <BlackPieceStand />
+          </Col>
+        </Row>
+      </Grid>
     </div>
   );
 };
--- frontend/components/board.js
+++ frontend/components/board.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
 import Styles from '../styles/board.css';
 import PromoteModal from '../containers/modals/promoteModal';
 import Piece from '../containers/piece';
-import BlackPieceStand from '../containers/blackPieceStand';
-import WhitePieceStand from '../containers/whitePieceStand';
 
 const Board = ({ board }) => {
   const tbody = board.map((row, y) => {
@@ -23,22 +20,12 @@ const Board = ({ board }) => {
   });
 
   return (
-    <Grid>
+    <div>
       <PromoteModal />
-      <Row center="xs">
-        <Col xs={1}>
-          <WhitePieceStand />
-        </Col>
-        <Col xs={6}>
-          <table className="board" style={{backgroundColor: 'black'}}>
-            <tbody>{tbody}</tbody>
-          </table>
-        </Col>
-        <Col xs={1}>
-          <BlackPieceStand />
-        </Col>
-      </Row>
-    </Grid>
+      <table className="board" style={{backgroundColor: 'black'}}>
+        <tbody>{tbody}</tbody>
+      </table>
+    </div>
   );
 };
 
